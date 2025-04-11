033187462d149e84058f80e1ec89e80f17ae39de
Baiquan Fan <1252883+bqfan@users.noreply.github.com>
2025-04-11 21:04:51 +0300

    chore: remove test (expect) from stories

--- components/stories/Page.stories.tsx
+++ components/stories/Page.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta } from '@storybook/react';
-import { expect } from '@storybook/test';
+//
 import { userEvent, within } from '@storybook/testing-library';
 import { Page } from './Page';
 
@@ -12,12 +12,12 @@ export const LoggedIn = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = canvas.getByRole('button', { name: /Log in/i });
-    await expect(loginButton).toBeInTheDocument();
+   // await expect(loginButton).toBeInTheDocument();
     await userEvent.click(loginButton);
     // FIXME: await expect(loginButton).not.toBeInTheDocument();
 
     const logoutButton = canvas.getByRole('button', { name: /Log out/i });
-    await expect(logoutButton).toBeInTheDocument();
+    //await expect(logoutButton).toBeInTheDocument();
   },
 };
 
